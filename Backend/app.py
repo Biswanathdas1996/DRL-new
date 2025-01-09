1cc91e254ccadd78fc274bfd8d31a0bba714fd6f
Biswanath <cv.biswanath@gmail.com>
2025-01-10 00:21:50 +0530

    ok

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,8 @@
 from main.nlq import nlq
-from helper.utils import convert_string_to_json, convert_to_json, add_query_to_json
-from sql.index import  execute_query
+from helper.utils import add_query_to_json
 from flask import Flask, request, jsonify, send_file, Response
 from gpt.analiticts import getAnalitics, call_gpt
 from flask_cors import CORS
-from vector_db.vector_db import delete_collection, upload_files, list_collections, search_data
-from vector_db.fine_chunking import fine_chunking
 import os
 from helper.gpt import extract_image
 from sql.db import generate_erd_from, execute_sql_query
@@ -24,10 +21,7 @@ if __name__ == "__main__":
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
     CORS(app)
 
-    app.config['UPLOAD_FOLDER'] = 'vector_db/uploads'
-    app.config['IMG_UPLOAD_FOLDER'] = 'asset/uploads'
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    os.makedirs(app.config['IMG_UPLOAD_FOLDER'], exist_ok=True)
+    
     @app.before_request
     def before_request():
         custom_header = request.headers.get('X-Ai-Model')
@@ -36,22 +30,9 @@ if __name__ == "__main__":
             os.environ["X-Ai-Model"] = custom_header
             print(f"X-Ai-Model: {custom_header}")
 
-    @app.route('/query-mock', methods=['POST'])
-    def query_mock():
-        data = request.json
-        user_question = data.get('question')
-        if not user_question:
-            return jsonify({"error": "No question provided"}), 400
-        try:
-            with open('data/mock/tabuler.json', 'r') as file:
-                result_json = file.read()
-                print("Mock Result:", result_json)
-                return result_json
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-
-    @app.route('/save-query', methods=['POST'])
-    def save_query():
+   
+@app.route('/save-query', methods=['POST'])
+def save_query():
         data = request.json
         q_data = data.get('data')
         if not q_data:
@@ -64,8 +45,8 @@ if __name__ == "__main__":
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
-    @app.route('/query-list', methods=['GET'])
-    def get_all_query():
+@app.route('/query-list', methods=['GET'])
+def get_all_query():
         try:
             with open('query_storage/query.json', 'r') as file:
                 result_json = file.read()
@@ -74,12 +55,8 @@ if __name__ == "__main__":
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
-
-
-
-
-    @app.route('/query', methods=['POST'])
-    def query():
+@app.route('/query', methods=['POST'])
+def query():
         data = request.json
         user_question = data.get('question')
         working_table_description = data.get('working_table_description')
@@ -106,9 +83,8 @@ if __name__ == "__main__":
             except Exception as e:
                 return jsonify({"error": str(e)}), 500
         
-
-    @app.route('/analitics', methods=['POST'])
-    def analitics_data():
+@app.route('/analitics', methods=['POST'])
+def analitics_data():
         data = request.json
         result_json = data.get('result_json')
         if not result_json:
@@ -123,9 +99,8 @@ if __name__ == "__main__":
         except Exception as e:
             return jsonify({"error": str(e)}), 500
         
-
-    @app.route('/analitics-mock', methods=['POST'])
-    def analitics_data_mock():
+@app.route('/analitics-mock', methods=['POST'])
+def analitics_data_mock():
         data = request.json
         result_json = data.get('result_json')
         if not result_json:
@@ -139,9 +114,8 @@ if __name__ == "__main__":
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
-
-    @app.route('/exicute-raw-query', methods=['POST'])
-    def exicute_query():
+@app.route('/exicute-raw-query', methods=['POST'])
+def exicute_query():
         data = request.json
         user_sql_query = data.get('sql_query')
         if not user_sql_query:
@@ -174,11 +148,8 @@ if __name__ == "__main__":
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
-
-# ------------------------------------user story api --------------------------------------------
-
-    @app.route('/call-gpt', methods=['POST'])
-    def direct_gpt_call():
+@app.route('/call-gpt', methods=['POST'])
+def direct_gpt_call():
         data = request.json
         user_question = data.get('question')
         token_limit = data.get('token_limit')
@@ -192,76 +163,6 @@ if __name__ == "__main__":
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
-
-# ------------------------------------file upload --------------------------------------------
-
-# File upload endpoint with chroma db
-@app.route('/upload-collection-doc', methods=['POST'])
-def upload_files_data():
-    if 'files' not in request.files:
-        return "No files provided", 400
-    collection_name = request.form.get('collection_name')
-    files = request.files.getlist('files')
-    try:
-        result = upload_files(collection_name, files, app.config['UPLOAD_FOLDER'])
-        print("Result:=============>", result)
-        return jsonify({"message": result}), 200 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-# Search endpoint
-@app.route('/search', methods=['POST'])
-def search_file_data():
-    data = request.get_json()
-    query = data.get('query')
-    no_of_results = data.get('no_of_results')
-    collection_name = data.get('collection_name')
-    if_fine_chunking = data.get('fine_chunking')
-    if_gpt_summarize = data.get('if_gpt_summarize')
-    if not query:
-        return jsonify({"error": "No query provided"}), 400       
-    if not collection_name:
-        return jsonify({"error": "No collection_name provided"}), 400       
-    results = search_data(query, collection_name,no_of_results)
-
-    response_result = {"results": results}
-  
-    ### file chunking ######
-    if if_fine_chunking:
-        fine_results = fine_chunking(results['documents'],query, 100)
-        response_result["fine_results"] = fine_results
-       
-    if if_gpt_summarize:
-        gpt_results = call_gpt("You are an expert summrizer", f"find and list all the key points: \n{results['documents']}", 1000)
-        response_result["gpt_results"] = gpt_results
-        
-    return jsonify(response_result), 200
-
-
-# Search endpoint
-@app.route('/collection', methods=['GET'])
-def get_all_collection():
-    dlist_collections = list_collections()
-    return jsonify({"collections": dlist_collections}), 200
-
-
-# Search endpoint
-@app.route('/collection', methods=['DELETE'])
-def remove_collection():
-    data = request.get_json()
-    collection_name = data.get('collection_name')
-    result = delete_collection(collection_name)
-    return jsonify({"collections": result}), 200
-
-# ----------------------------mongo DB--------------------------------------------
-
-
-
-
-
-# --------------------------------------------------------------------------------
-
 @app.route('/extract-img', methods=['POST'])
 def extract_img_api():
     if 'file' not in request.files:
@@ -274,7 +175,6 @@ def extract_img_api():
         return jsonify({"details": img_details}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
 
 
 @app.route('/generate-erd-from-db', methods=['POST'])
@@ -309,7 +209,6 @@ def generate_erd_from_db():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-
 @app.route('/get-erd-image', methods=['GET'])
 def get_erd_img():
     try:
