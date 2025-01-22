4649d8d35a6be481dc5740e90975067e60740716
Biswanath <cv.biswanath@gmail.com>
2025-01-22 16:43:52 +0530

    <TextField         label="Search"

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -29,166 +29,129 @@ const CustomTable: React.FC<TableProps> = ({
   setData,
 }) => {
   const dispatch = useDispatch<AppDispatch>();
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState(0);
 
+  const shouldDisplayTable = (data: any) => {
+    return data.table1 && data.table1.length > 0;
+  };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
   const [paginationModel, setPaginationModel] = React.useState({
     page: 0,
     pageSize: 5,
     sortBy: "",
     order: "asc",
   });
-
-  if (data.length !== 0) {
-    return (
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
-        <TextField
-          label="Search"
-          variant="outlined"
-          size="small"
-          onChange={(e) => {
-            const searchTerm = e.target.value.toLowerCase();
-            const filteredData = data.filter((row) =>
+  const [table1Data, setTable1Data] = React.useState(data);
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TextField
+        label="Search"
+        variant="outlined"
+        size="small"
+        onChange={(e) => {
+          const searchTerms = e.target.value
+            .toLowerCase()
+            .split(",")
+            .map((term) => term.trim());
+          const filteredData = data.filter((row) =>
+            searchTerms.some((term) =>
               Object.values(row).some((value) =>
-                String(value).toLowerCase().includes(searchTerm)
+                String(value).toLowerCase().includes(term)
               )
-            );
-            setPaginationModel({ ...paginationModel, page: 0 });
-            if (setData) setData(filteredData);
-            chatId &&
-              dispatch(
-                updateResult({
-                  chatId,
-                  result: filteredData,
-                })
-              );
-          }}
-          sx={{ marginBottom: 2 }}
-        />
-        <TableContainer sx={{ maxHeight: 440 }}>
-          <MuiTable stickyHeader aria-label="sticky table">
-            <TableHead style={{ height: 10 }}>
-              <TableRow style={{ height: 10 }}>
-                {data.length > 0 &&
-                  Object.keys(data[0]).map((columnName, index) => (
-                    <TableCell
-                      key={index}
-                      style={{
-                        minWidth: 170,
-                        background: "rgb(51 51 51 / 86%)",
-                        color: "white",
-                        height: 10,
-                        padding: "5px",
-                        margin: 0,
-                        textAlign: "center",
-                        borderRight: "2px solid #ffffff",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => {
-                        const isAsc =
-                          paginationModel.sortBy === columnName &&
-                          paginationModel.order === "asc";
-                        const order = isAsc ? "desc" : "asc";
-                        const sortedData = [...data].sort((a, b) => {
-                          const aValue = a[columnName];
-                          const bValue = b[columnName];
-                          if (
-                            typeof aValue === "number" &&
-                            typeof bValue === "number"
-                          ) {
-                            return order === "asc"
-                              ? aValue - bValue
-                              : bValue - aValue;
-                          } else {
-                            return order === "asc"
-                              ? String(aValue).localeCompare(String(bValue))
-                              : String(bValue).localeCompare(String(aValue));
-                          }
-                        });
-                        if (setData) setData(sortedData);
-                        setPaginationModel({
-                          ...paginationModel,
-                          sortBy: columnName,
-                          order,
-                        });
-                      }}
-                    >
-                      {columnName
-                        .replace(/_/g, " ")
-                        .replace(/^\w/, (c) => c.toUpperCase())}
-                      {paginationModel.sortBy === columnName
-                        ? paginationModel.order === "asc"
-                          ? " 🔼"
-                          : " 🔽"
-                        : ""}
-                    </TableCell>
-                  ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data
-                .slice(
-                  paginationModel.page * paginationModel.pageSize,
-                  paginationModel.page * paginationModel.pageSize +
-                    paginationModel.pageSize
-                )
-                .map((row: Record<string, string | number>, rowIndex) => (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={rowIndex}
+            )
+          );
+          setPaginationModel({ ...paginationModel, page: 0 });
+          setTable1Data(filteredData);
+        }}
+        sx={{ marginBottom: 2 }}
+      />
+      <TableContainer sx={{ maxHeight: 440, width: "100%" }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead style={{ height: 10 }}>
+            <TableRow style={{ height: 10 }}>
+              {table1Data.length > 0 &&
+                Object.keys(table1Data[0]).map((columnName, index) => (
+                  <TableCell
+                    key={index}
                     style={{
-                      backgroundColor:
-                        rowIndex % 2 === 0 ? "#a8a8a866" : "#f1f1f1",
+                      minWidth: 170,
+                      background: "rgb(51 51 51 / 86%)",
+                      color: "white",
+                      height: 10,
+                      padding: "5px",
+                      margin: 0,
+                      textAlign: "center",
+                      borderRight: "2px solid #ffffff",
                     }}
                   >
-                    {Object.values(row).map((value, cellIndex) => (
-                      <TableCell
-                        key={cellIndex}
-                        align="center"
-                        style={{
-                          borderRight: "2px solid #ffffff",
-                          padding: 5,
-                        }}
-                      >
-                        {isNaN(Number(value))
-                          ? value
-                          : Number(value).toFixed(2)}
-                      </TableCell>
-                    ))}
-                  </TableRow>
+                    {columnName
+                      .replace(/_/g, " ")
+                      .replace(/^\w/, (c) => c.toUpperCase())}
+                  </TableCell>
                 ))}
-            </TableBody>
-          </MuiTable>
-        </TableContainer>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              colSpan={3}
-              count={data.length}
-              rowsPerPage={paginationModel.pageSize}
-              page={paginationModel.page}
-              SelectProps={{
-                inputProps: {
-                  "aria-label": "rows per page",
-                },
-                native: true,
-              }}
-              onPageChange={(event, newPage) => {
-                setPaginationModel((prev) => ({ ...prev, page: newPage }));
-              }}
-              onRowsPerPageChange={(event) => {
-                setPaginationModel((prev) => ({
-                  ...prev,
-                  pageSize: parseInt(event.target.value, 10),
-                }));
-              }}
-            />
-          </TableRow>
-        </TableFooter>
-      </Paper>
-    );
-  }
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {table1Data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row: Record<string, string | number>, rowIndex) => (
+                <TableRow
+                  hover
+                  role="checkbox"
+                  tabIndex={-1}
+                  key={rowIndex}
+                  style={{
+                    backgroundColor:
+                      rowIndex % 2 === 0 ? "#a8a8a866" : "#f1f1f1",
+                  }}
+                >
+                  {Object.values(row).map((value, cellIndex) => (
+                    <TableCell
+                      key={cellIndex}
+                      align="center"
+                      style={{
+                        borderRight: "2px solid #ffffff",
+                        padding: 5,
+                      }}
+                    >
+                      {isNaN(Number(value)) ? value : Number(value).toFixed(2)}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TableFooter>
+        <TableRow>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            colSpan={3}
+            count={table1Data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            SelectProps={{
+              inputProps: {
+                "aria-label": "rows per page",
+              },
+              native: true,
+            }}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </TableRow>
+      </TableFooter>
+    </Paper>
+  );
 };
 
 const paginationModel = { page: 0, pageSize: 5, sortBy: "", order: "asc" };
