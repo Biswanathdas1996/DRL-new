903768daf952ef184f11b68fc7f9cd2b3e8afd27
Biswanath <cv.biswanath@gmail.com>
2025-02-04 12:06:06 +0530

    ok

--- src/hook/useQueryFilter.ts
+++ src/hook/useQueryFilter.ts
@@ -3,8 +3,9 @@ import filterData from "../string/filter.json";
 export const useQueryFilter = (query: string) => {
   let filteredQuery = query;
   filterData.forEach((filter) => {
-    if (query.includes(filter.find)) {
-      filteredQuery = filteredQuery.replace(filter.find, filter.replace);
+    const regex = new RegExp(filter.find, "gi");
+    if (regex.test(query)) {
+      filteredQuery = filteredQuery.replace(regex, filter.replace);
     }
   });
   return filteredQuery;
